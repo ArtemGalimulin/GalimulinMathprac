7058fc91730562774222531e58984af277785a08
ArtemGalimulin <artem_utro@mail.ru>
2026-05-07 13:10:21 +0300

    mesh done

--- macro/mesh.cpp
+++ macro/mesh.cpp
@@ -1,84 +1,101 @@
-// -----------------------------------------------------------------------------
-//
-//  Gmsh C++ tutorial 13
-//
-//  Remeshing an STL file without an underlying CAD model
-//
-// -----------------------------------------------------------------------------
-
 #include <set>
 #include <cmath>
 #include <gmsh.h>
 
-int main(int argc, char **argv)
-{
+int main(int argc, char** argv) {
   gmsh::initialize();
+  gmsh::model::add("Model");
 
-  gmsh::model::add("t13");
-
-  // Let's merge an STL mesh that we would like to remesh (from the parent
-  // directory):
   try {
     gmsh::merge("../dodge_wrap.stl");
-  } catch(...) {
+  } catch (...) {
     gmsh::logger::write("Could not load STL mesh: bye!");
     gmsh::finalize();
     return 0;
   }
 
-  // We first classify ("color") the surfaces by splitting the original surface
-  // along sharp geometrical features. This will create new discrete surfaces,
-  // curves and points.
+  gmsh::model::mesh::classifySurfaces(80 * M_PI / 180., true, true,
+                                      180 * M_PI / 180.);
+  gmsh::model::mesh::createGeometry();
 
-
-  // Angle between two triangles above which an edge is considered as sharp:
-  double angle = 40;
-
-  // For complex geometries, patches can be too complex, too elongated or too
-  // large to be parametrized; setting the following option will force the
-  // creation of patches that are amenable to reparametrization:
-  bool forceParametrizablePatches = true;
-
-  // For open surfaces include the boundary edges in the classification process:
-  bool includeBoundary = true;
-
-  // Force curves to be split on given angle:
-  double curveAngle = 180;
-
-  gmsh::model::mesh::classifySurfaces(angle * M_PI / 180., includeBoundary,
-                                      forceParametrizablePatches,
-                                      curveAngle * M_PI / 180.);
-
-  // Create a geometry for all the discrete curves and surfaces in the mesh, by
-  // computing a parametrization for each one
-  // gmsh::model::mesh::createGeometry();
-
-  // Create a volume from all the surfaces
-  std::vector<std::pair<int, int> > s;
+  std::vector<std::pair<int, int>> s;
   gmsh::model::getEntities(s, 2);
   std::vector<int> sl;
-  for(auto surf : s) sl.push_back(surf.second);
-  int l = gmsh::model::geo::addSurfaceLoop(sl);
-  gmsh::model::geo::addVolume({l});
+  for (auto surf : s) sl.push_back(surf.second);
+  int carLoop = gmsh::model::geo::addSurfaceLoop(sl);
+  gmsh::model::geo::addVolume({carLoop});
+
+  // Машина: X[-2.32, 0.43]  Y[-2.56, 3.25]  Z[0.0, 2.17]
+  // Увеличиваем отступы до 2 размеров машины
+  double xmin = -2.32 - 5.5; // -7.82
+  double xmax = 0.43 + 5.5; //  5.93
+  double ymin = -2.56 - 8.0; // -10.56  вход
+  double ymax = 3.25 + 15.0; //  18.25  выход
+  double zmin = -0.5;
+  double zmax = 2.17 + 5.5; //  7.67
+
+  int p1 = gmsh::model::geo::addPoint(xmin, ymin, zmin);
+  int p2 = gmsh::model::geo::addPoint(xmax, ymin, zmin);
+  int p3 = gmsh::model::geo::addPoint(xmax, ymax, zmin);
+  int p4 = gmsh::model::geo::addPoint(xmin, ymax, zmin);
+  int p5 = gmsh::model::geo::addPoint(xmin, ymin, zmax);
+  int p6 = gmsh::model::geo::addPoint(xmax, ymin, zmax);
+  int p7 = gmsh::model::geo::addPoint(xmax, ymax, zmax);
+  int p8 = gmsh::model::geo::addPoint(xmin, ymax, zmax);
+
+  int l1 = gmsh::model::geo::addLine(p1, p2);
+  int l2 = gmsh::model::geo::addLine(p2, p3);
+  int l3 = gmsh::model::geo::addLine(p3, p4);
+  int l4 = gmsh::model::geo::addLine(p4, p1);
+  int l5 = gmsh::model::geo::addLine(p5, p6);
+  int l6 = gmsh::model::geo::addLine(p6, p7);
+  int l7 = gmsh::model::geo::addLine(p7, p8);
+  int l8 = gmsh::model::geo::addLine(p8, p5);
+  int l9 = gmsh::model::geo::addLine(p1, p5);
+  int l10 = gmsh::model::geo::addLine(p2, p6);
+  int l11 = gmsh::model::geo::addLine(p3, p7);
+  int l12 = gmsh::model::geo::addLine(p4, p8);
+
+  int cl1 = gmsh::model::geo::addCurveLoop({l1, l2, l3, l4});
+  int cl2 = gmsh::model::geo::addCurveLoop({l5, l6, l7, l8});
+  int cl3 = gmsh::model::geo::addCurveLoop({l1, l10, -l5, -l9});
+  int cl4 = gmsh::model::geo::addCurveLoop({l2, l11, -l6, -l10});
+  int cl5 = gmsh::model::geo::addCurveLoop({l3, l12, -l7, -l11});
+  int cl6 = gmsh::model::geo::addCurveLoop({l4, l9, -l8, -l12});
+
+  int s1 = gmsh::model::geo::addPlaneSurface({cl1}); // пол
+  int s2 = gmsh::model::geo::addPlaneSurface({cl2}); // потолок
+  int s3 = gmsh::model::geo::addPlaneSurface({cl3}); // вход ymin
+  int s4 = gmsh::model::geo::addPlaneSurface({cl4}); // правая стенка
+  int s5 = gmsh::model::geo::addPlaneSurface({cl5}); // выход ymax
+  int s6 = gmsh::model::geo::addPlaneSurface({cl6}); // левая стенка
+
+  int tunnelLoop = gmsh::model::geo::addSurfaceLoop({s1, s2, s3, s4, s5, s6});
+  gmsh::model::geo::addVolume({tunnelLoop, carLoop});
 
   gmsh::model::geo::synchronize();
 
-  // We specify element sizes imposed by a size field, just because we can :-)
-  bool funny = false; // false;
+  // Физические группы — границы для FEniCS
+  gmsh::model::addPhysicalGroup(2, {s3}, 1, "inlet"); // вход потока
+  gmsh::model::addPhysicalGroup(2, {s5}, 2, "outlet"); // выход потока
+  gmsh::model::addPhysicalGroup(2, {s1, s2, s4, s6}, 3, "walls"); // стенки туннеля
+  gmsh::model::addPhysicalGroup(2, sl, 4, "car_wall"); // поверхность машины (no-slip)
+  // Объём воздуха
+  gmsh::model::addPhysicalGroup(3, {2}, 5, "air"); // область расчёта НС
+
   int f = gmsh::model::mesh::field::add("MathEval");
-  if(funny)
-    gmsh::model::mesh::field::setString(f, "F", "2*sin((x+y)/5) + 3");
-  else
-    gmsh::model::mesh::field::setString(f, "F", "4");
+  gmsh::model::mesh::field::setString(f, "F", "0.3");
   gmsh::model::mesh::field::setAsBackgroundMesh(f);
 
+  gmsh::option::setNumber("Mesh.MaxNumThreads3D", 8);
+  gmsh::option::setNumber("Mesh.Algorithm3D", 1);
+  gmsh::option::setNumber("Mesh.Optimize", 0);
+
   gmsh::model::mesh::generate(3);
+  gmsh::write("pickup.msh");
 
-  gmsh::write("t4.msh");
-
-  // Launch the GUI to see the results:
   std::set<std::string> args(argv, argv + argc);
-  if(!args.count("-nopopup")) gmsh::fltk::run();
+  if (!args.count("-nopopup")) gmsh::fltk::run();
 
   gmsh::finalize();
   return 0;
